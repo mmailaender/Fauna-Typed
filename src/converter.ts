d5622054438e1f54dbeae6985d559714ac09de24
Aditya Vaishnav <firehudson@gmail.com>
2023-04-17 06:06:35 +0000

    Add cosmiconfig

--- src/converter.ts
+++ src/converter.ts
@@ -1,100 +1,115 @@
-import fs from 'fs';
-import { getPascalCaseString } from './helper';
-import { TopLevelInterfaces, createTypedefsMethods } from './util';
+// import fs from 'fs';
+import {cosmiconfig} from 'cosmiconfig'
+// import { getPascalCaseString } from './helper';
+// import { TopLevelInterfaces, createTypedefsMethods } from './util';
 
-const schemaStr = fs.readFileSync(
-  `../../../../../..${process.env.INIT_CWD}/${process.argv[process.argv.length - 1]}`,
-  { encoding: 'utf-8' }
-);
-const schema = JSON.parse(schemaStr);
-const types = {
-  int: 'number',
-  float: 'number',
-  string: 'string',
-};
+const explorer = cosmiconfig('fqlx');
 
-let typeSchema = '';
-let queryInterfaceKeyValue = '';
-
-const getKeyType = (value: any, fieldKey: string) => {
-  // return type for embedded onject
-  if (typeof value === 'object') {
-    return getPascalCaseString(fieldKey);
-  }
-
-  return value
-    .split('|')
-    .map((s: string) => types[s.trim() as keyof typeof types] || s)
-    .join(' | ');
-};
-
-const createInterface = (key: string, fields: object) => {
-  const keyInPascalCase = getPascalCaseString(key);
-  const fieldsEntries = Object.entries(fields);
-
-  let mainInterfaceKeyValue = '';
-  let inputInterfaceKeyValue = '';
-
-  fieldsEntries.forEach(fieldEntry => {
-    const fieldValue = fieldEntry[1];
-    const fieldKey = fieldEntry[0];
-
-    const fieldKeyPascalCase = getPascalCaseString(fieldKey);
-    const valueType = getKeyType(fieldValue, fieldKey);
-
-    // Creating interface for embedded objects
-    if (typeof fieldValue === 'object') {
-      createInterface(fieldKey, fieldValue);
-
-      queryInterfaceKeyValue = queryInterfaceKeyValue.concat(
-        `${fieldKeyPascalCase}:  ${fieldKeyPascalCase}Methods;\n`
-      );
-    }
-
-    mainInterfaceKeyValue = mainInterfaceKeyValue.concat(
-      `${fieldKey}: ${valueType};\n`
-    );
-
-    if (fieldKey.toLowerCase() !== 'id') {
-      inputInterfaceKeyValue = inputInterfaceKeyValue.concat(
-        `${fieldKey}: ${valueType};\n`
-      );
-    }
+explorer.search()
+  .then((result) => {
+    console.log("==========result=======", result)
+    // result.config is the parsed configuration object.
+    // result.filepath is the path to the config file that was found.
+    // result.isEmpty is true if there was nothing to parse in the config file.
+  })
+  .catch((error) => {
+    console.log("==========error=======", error)
+    // Do something constructive.
   });
 
-  typeSchema = typeSchema.concat(`export interface ${keyInPascalCase} {
-    ${mainInterfaceKeyValue}
-  }\n\n
-  export interface ${keyInPascalCase}Input {
-    ${inputInterfaceKeyValue}
-  } \n\n
-  ${createTypedefsMethods(keyInPascalCase)}
-  `);
-};
+// const schemaStr = fs.readFileSync(
+//   `../../../../../..${process.env.INIT_CWD}/${process.argv[process.argv.length - 1]}`,
+//   { encoding: 'utf-8' }
+// );
+// const schema = JSON.parse(schemaStr);
+// const types = {
+//   int: 'number',
+//   float: 'number',
+//   string: 'string',
+// };
 
-const generateTypeDefs = () => {
-  const schemaKeys = Object.keys(schema);
+// let typeSchema = '';
+// let queryInterfaceKeyValue = '';
 
-  schemaKeys.forEach((key: string) => {
-    const keyInPascalCase = getPascalCaseString(key);
+// const getKeyType = (value: any, fieldKey: string) => {
+//   // return type for embedded onject
+//   if (typeof value === 'object') {
+//     return getPascalCaseString(fieldKey);
+//   }
 
-    queryInterfaceKeyValue = queryInterfaceKeyValue.concat(
-      `${keyInPascalCase}: ${keyInPascalCase}Methods;\n`
-    );
-    createInterface(key, schema[key as keyof typeof schema].fields);
-  });
+//   return value
+//     .split('|')
+//     .map((s: string) => types[s.trim() as keyof typeof types] || s)
+//     .join(' | ');
+// };
 
-  typeSchema = typeSchema.concat(`export interface Query {
-    ${queryInterfaceKeyValue}
-  }`);
-};
+// const createInterface = (key: string, fields: object) => {
+//   const keyInPascalCase = getPascalCaseString(key);
+//   const fieldsEntries = Object.entries(fields);
 
-generateTypeDefs();
+//   let mainInterfaceKeyValue = '';
+//   let inputInterfaceKeyValue = '';
 
-fs.writeFileSync(
-  './src/generated/typedefs.ts',
-  TopLevelInterfaces.concat(typeSchema),
-  {
-    encoding: 'utf-8',
-  }
-);
+//   fieldsEntries.forEach(fieldEntry => {
+//     const fieldValue = fieldEntry[1];
+//     const fieldKey = fieldEntry[0];
+
+//     const fieldKeyPascalCase = getPascalCaseString(fieldKey);
+//     const valueType = getKeyType(fieldValue, fieldKey);
+
+//     // Creating interface for embedded objects
+//     if (typeof fieldValue === 'object') {
+//       createInterface(fieldKey, fieldValue);
+
+//       queryInterfaceKeyValue = queryInterfaceKeyValue.concat(
+//         `${fieldKeyPascalCase}:  ${fieldKeyPascalCase}Methods;\n`
+//       );
+//     }
+
+//     mainInterfaceKeyValue = mainInterfaceKeyValue.concat(
+//       `${fieldKey}: ${valueType};\n`
+//     );
+
+//     if (fieldKey.toLowerCase() !== 'id') {
+//       inputInterfaceKeyValue = inputInterfaceKeyValue.concat(
+//         `${fieldKey}: ${valueType};\n`
+//       );
+//     }
+//   });
+
+//   typeSchema = typeSchema.concat(`export interface ${keyInPascalCase} {
+//     ${mainInterfaceKeyValue}
+//   }\n\n
+//   export interface ${keyInPascalCase}Input {
+//     ${inputInterfaceKeyValue}
+//   } \n\n
+//   ${createTypedefsMethods(keyInPascalCase)}
+//   `);
+// };
+
+// const generateTypeDefs = () => {
+//   const schemaKeys = Object.keys(schema);
+
+//   schemaKeys.forEach((key: string) => {
+//     const keyInPascalCase = getPascalCaseString(key);
+
+//     queryInterfaceKeyValue = queryInterfaceKeyValue.concat(
+//       `${keyInPascalCase}: ${keyInPascalCase}Methods;\n`
+//     );
+//     createInterface(key, schema[key as keyof typeof schema].fields);
+//   });
+
+//   typeSchema = typeSchema.concat(`export interface Query {
+//     ${queryInterfaceKeyValue}
+//   }`);
+// };
+
+// generateTypeDefs();
+
+// fs.writeFileSync(
+//   './src/generated/typedefs.ts',
+//   TopLevelInterfaces.concat(typeSchema),
+//   {
+//     encoding: 'utf-8',
+//   }
+// );
