fe3e01b318e6a5eafd8f083a5582ac9c689e5401
Aditya Vaishnav <firehudson@gmail.com>
2023-04-20 09:10:28 +0000

    Cleanup

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.PWD': JSON.stringify(process.env.PWD),
-      process: 'process/browser',
-    }),
-  ],
-};
