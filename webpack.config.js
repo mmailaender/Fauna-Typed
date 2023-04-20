acf86022d04cb4690f6a15998b979358489dd412
Aditya Vaishnav <firehudson@gmail.com>
2023-04-20 06:46:29 +0000

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       'process.env.PWD': JSON.stringify(process.env.PWD),
+      process: 'process/browser',
     }),
   ],
 };
