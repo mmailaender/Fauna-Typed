d8f510fb519957e8318863104885d78cd1e0011f
Aditya Vaishnav <firehudson@gmail.com>
2023-04-25 11:56:36 +0000

    Update util to generate nested fields

--- src/collectionsWithFields.ts
+++ src/collectionsWithFields.ts
@@ -1,33 +1,41 @@
+type Field = { name: string; type: string };
 
 type CollectionsWithFieldsType = {
   name: string;
-  fields: { name: string; type: string }[];
+  fields: Field[];
 };
 
-export const getCollectionsWithFields = (schema: {
+type GetCollectionsWithFieldsArgs = {
   [key: string]: { fields: { [key: string]: any } };
-}): CollectionsWithFieldsType[] => {
+};
+
+const getFields = (fields: { [key: string]: any }): Field[] => {
+  return Object.keys(fields).map(fieldKey => {
+    const fieldValue = fields[fieldKey as keyof typeof fields];
+
+    if (typeof fieldValue === 'object') {
+      return {
+        name: fieldKey,
+        type: 'object',
+        fields: getFields(fieldValue),
+      };
+    }
+    return {
+      name: fieldKey,
+      type: fieldValue,
+    };
+  });
+};
+
+export const getCollectionsWithFields = (
+  schema: GetCollectionsWithFieldsArgs
+): CollectionsWithFieldsType[] => {
   return Object.keys(schema).map(collectionName => {
     const fields = schema[collectionName as keyof typeof schema].fields;
 
-    const mappedFields = Object.keys(fields).map(fieldKey => {
-      const fieldValue = fields[fieldKey as keyof typeof fields];
-
-      if (typeof fieldValue === 'object') {
-        return {
-          name: fieldKey,
-          type: 'object',
-        };
-      }
-      return {
-        name: fieldKey,
-        type: fieldValue,
-      };
-    });
-
     return {
       name: collectionName,
-      fields: mappedFields,
+      fields: getFields(fields),
     };
   });
 };
