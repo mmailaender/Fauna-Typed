ef2c94409181e32cc4f7168c66ec8cc7c77ff747
Madhav <madhav.kabra@dechea.com>
2023-05-01 06:05:55 +0000

    Log state

--- src/client.ts
+++ src/client.ts
@@ -8,10 +8,11 @@ class FqlxClient {
 
   static getClient() {
     if (!this.client) {
+      console.log('fqlxStore.getState()========', fqlxStore.getState());
       const secret = fqlxStore.getState().fqlxSecret;
       console.log('========secret=====', secret);
       if (secret) {
-      console.log('========new client creating=====', secret);
+        console.log('========new client creating=====', secret);
 
         this.client = new Client({
           endpoint: endpoints.preview,
