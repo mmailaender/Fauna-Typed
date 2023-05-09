ffa1e2694770006b84d09249a143d4eb2dd0380e
Madhav <madhav.kabra@dechea.com>
2023-05-09 16:33:42 +0000

    Cleanup

--- src/FqlxProvider.tsx
+++ src/FqlxProvider.tsx
@@ -6,10 +6,10 @@ import { configState, useConfigStore } from './configStore';
 export const FqlxProvider = ({
   config,
   children,
-}: // loader,
-{
+  loader,
+}: {
   config: { fqlxSecret: string };
-  // loader: React.ReactElement;
+  loader: React.ReactElement;
   children: React.ReactElement;
 }) => {
   const { setFqlxSecret, fqlxSecret } = useConfigStore(
@@ -18,25 +18,16 @@ export const FqlxProvider = ({
   console.log('config===========', config.fqlxSecret);
 
   useEffect(() => {
-    console.log('============in useEffect');
-
     if (config.fqlxSecret && config.fqlxSecret !== fqlxSecret) {
-      console.log('============updating secret');
       setFqlxSecret(config.fqlxSecret);
     }
   }, []);
 
   if (!config.fqlxSecret) {
-    console.log('============missing secret');
     throw new Error('Missing Fauna Secret');
   }
 
-  // if (!fqlxSecret) {
-  //   console.log('============loading');
-  //   return <div>Loading...</div>;
-  // }
-
   console.log('secret in store========', fqlxSecret);
 
-  return <div>{!fqlxSecret ? 'Loading...' : children}</div>;
+  return <div>{!fqlxSecret ? loader : children}</div>;
 };
