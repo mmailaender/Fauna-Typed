2c607ca521084061df30ec6c307ff5c77b53c3ce
Aditya Vaishnav <firehudson@gmail.com>
2023-05-10 15:10:20 +0000

    Add log

--- src/client.ts
+++ src/client.ts
@@ -9,6 +9,7 @@ class FqlxClient {
 
   getClient(): Client {
     if (!this.client) {
+      console.log('store======', this.store.getState());
       const secret = this.store.getState().fqlxSecret;
       const endpoint = (this.store.getState().fqlxEndpoint as unknown) as URL;
 
