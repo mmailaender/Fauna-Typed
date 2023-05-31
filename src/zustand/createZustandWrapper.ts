3a2d63bd09ec4a32a57eca393f5f51615222417e
Madhav <madhav.kabra@dechea.com>
2023-05-31 11:49:34 +0000

    Persist zustand store with localStorage

--- src/zustand/createZustandWrapper.ts
+++ src/zustand/createZustandWrapper.ts
@@ -1,6 +1,7 @@
 'use client';
 
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { StateKeys, ZustandState, ZustandStore } from './interface';
 import {
   handleCreateDocument,
@@ -20,49 +21,52 @@ import {
 } from './deleteActions';
 
 export const createZustandWrapper = (): ZustandStore => {
-  const store = create<ZustandState>(
-    (set, get) =>
-      ({
-        // temp: [],
-        fetchingPromise: {},
-        activeQuery: {},
-        create: <T>(inputData: T, collection: StateKeys) => {
-          handleCreateDocument<T>(set, inputData, collection);
-        },
-        onCreateSuccess: (id: string, resId: string, collection: StateKeys) =>
-          handleCreateDocumentSuccess(set, id, resId, collection),
+  const store = create<ZustandState>()(
+    persist(
+      (set, get) =>
+        ({
+          // temp: [],
+          fetchingPromise: {},
+          activeQuery: {},
+          create: <T>(inputData: T, collection: StateKeys) => {
+            handleCreateDocument<T>(set, inputData, collection);
+          },
+          onCreateSuccess: (id: string, resId: string, collection: StateKeys) =>
+            handleCreateDocumentSuccess(set, id, resId, collection),
 
-        onCreateFailed: (id: string, collection: StateKeys) =>
-          handleCreateDocumentFailed(set, id, collection),
+          onCreateFailed: (id: string, collection: StateKeys) =>
+            handleCreateDocumentFailed(set, id, collection),
 
-        getStateById: <T>(id: string, collection: StateKeys) => {
-          return handleGetStateById<T>(get, id, collection);
-        },
+          getStateById: <T>(id: string, collection: StateKeys) => {
+            return handleGetStateById<T>(get, id, collection);
+          },
 
-        update: <T>(id: string, inputData: T, collection: StateKeys) => {
-          handleUpdateDocument<T>(set, id, inputData, collection);
-        },
+          update: <T>(id: string, inputData: T, collection: StateKeys) => {
+            handleUpdateDocument<T>(set, id, inputData, collection);
+          },
 
-        onUpdateSuccess: (id: string, _collection: StateKeys) => {
-          handleUpdateDocumentSuccess(set, id);
-        },
+          onUpdateSuccess: (id: string, _collection: StateKeys) => {
+            handleUpdateDocumentSuccess(set, id);
+          },
 
-        onUpdateFailed: (id: string, collection: StateKeys) => {
-          handleUpdateDocumentError(set, id, collection);
-        },
+          onUpdateFailed: (id: string, collection: StateKeys) => {
+            handleUpdateDocumentError(set, id, collection);
+          },
 
-        delete: (id: string, collection: StateKeys) => {
-          handleDeleteDocument(set, id, collection);
-        },
+          delete: (id: string, collection: StateKeys) => {
+            handleDeleteDocument(set, id, collection);
+          },
 
-        onDeleteSuccess: (id: string) => {
-          handleDeleteDocumentSuccess(set, id);
-        },
+          onDeleteSuccess: (id: string) => {
+            handleDeleteDocumentSuccess(set, id);
+          },
 
-        onDeleteFailed: (id: string, collection: StateKeys) => {
-          handleDeleteDocumentError(set, id, collection);
-        },
-      } as ZustandState)
+          onDeleteFailed: (id: string, collection: StateKeys) => {
+            handleDeleteDocumentError(set, id, collection);
+          },
+        } as ZustandState),
+      { name: 'fauna-react' }
+    )
   );
 
   return store;
