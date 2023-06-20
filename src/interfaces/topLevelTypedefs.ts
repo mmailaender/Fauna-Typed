0edfc614de32b13a051a2bb2a6cd38e42762521a
Madhav <madhav.kabra@dechea.com>
2023-06-20 07:45:29 +0000

    Add interfaces for map and distinct method

--- src/interfaces/topLevelTypedefs.ts
+++ src/interfaces/topLevelTypedefs.ts
@@ -20,32 +20,88 @@ export interface ExecMethods<T> {
   exec(): T;
 }
 
-export interface CreateMethods<T> extends ProjectionMethods<T, T>, PromisifyExecMethods<T> {
-  
+export interface CreateMethods<T>
+  extends ProjectionMethods<T, T>,
+    PromisifyExecMethods<T> {}
+
+export type OrderMethodInput<T> =
+  | `asc(.${string & keyof T})`
+  | `desc(.${string & keyof T})`;
+
+export interface OrderMethods<T>
+  extends FqlxFirst<T>,
+    ProjectionMethods<T, PaginateData<T>>,
+    ExecMethods<PaginateData<T>> {}
+
+export interface DistinctMethod<T> extends ExecMethods<PaginateData<T>> {
+  /**
+   * The distinct() method returns the unique elements in the array.
+   *
+   * @returns Unique elements in the array.
+   *
+   * @example
+   * query.Address.all().map(address => address.country).distinct().exec();
+   * OR
+   * query.Address.where(data => data.country == "US").map(address => address.city).distinct().exec();
+   *
+   * @see {@link https://fqlx-beta--fauna-docs.netlify.app/fqlx/beta/reference/builtin_functions/array/distinct#description See more...}
+   */
+  distinct(): BaseDistinctMethod<T>;
 }
 
-export type OrderMethodInput<T> = `asc(.${string & keyof T})` | `desc(.${string & keyof T})`
+export interface BaseDistinctMethod<T> extends ExecMethods<PaginateData<T>> {}
+
+
+export interface BaseMapMethod<T> extends ExecMethods<PaginateData<T>>, DistinctMethod<T> {}
+
+export interface MapMethod<T>  {
+  /**
+   * Create an Array by executing a function for each Array element.
+   *
+   * @param callbackFn Anonymous Function to invoke for each element of the calling Array. The function accepts an Array element as an argument.
+   *
+   * @returns A new Array that is the result of function invoked on each element of the calling Array.
+   *
+   * @example
+   * query.Address.all().map(address => address.country).exec();
+   * OR
+   * query.Address.where(data => data.country == "US").map(address => address.city).exec();
+   *
+   * @see {@link https://fqlx-beta--fauna-docs.netlify.app/fqlx/beta/reference/builtin_functions/array/map#description See more...}
+   */
+  map(
+    callbackFn: (data: T) => string | boolean | number | Partial<T>
+  ): BaseMapMethod<T>;
+}
 
 export interface FqlxOrder<T> extends ExecMethods<PaginateData<T>> {
   /**
-   * 
+   *
    * order method creates a Set by applying an Ordering to the values of this Set, and returns the new Set.
-   * 
+   *
    * @param {"asc(.key)" | "desc(.key)"} inputOrder creates Set with ordering applied.
-   * 
+   *
    * @returns {OrderMethods<T>} returns the new Set with ordering applied.
-   * 
+   *
    * @example
    * query.Address.all().order("asc(.id)").exec()
    * query.Address.all().order("desc(.id)").exec()
    * query.Address.all().where((data) => data.id != 123).order("asc(.id)").exec()
    * query.Address.all().where((data) => data.id != 123).order("desc(.id)").exec()
-   * 
+   *
    * @see {@link https://fqlx-beta--fauna-docs.netlify.app/fqlx/beta/reference/schema_entities/set/order#signature See more...}
    *
    */
   order(inputOrder: OrderMethodInput<T>): OrderMethods<T>;
 }
+
+export interface FirstWhereMethods<T>
+  extends ProjectionMethods<T, T>,
+    ExecMethods<T> {}
+
+export interface FirstMethods<T>
+  extends ProjectionMethods<T, T>,
+    ExecMethods<T> {}
 
 export interface FqlxFirst<T> {
   /**
@@ -63,18 +119,24 @@ export interface FqlxFirst<T> {
   first(): FirstMethods<T>;
 }
 
-export interface WhereMethods<T> extends FqlxOrder<T>, ProjectionMethods<T, PaginateData<T>>, ExecMethods<PaginateData<T>> {}
+export interface WhereMethods<T>
+  extends FqlxOrder<T>,
+    MapMethod<T>,
+    ProjectionMethods<T, PaginateData<T>>,
+    ExecMethods<PaginateData<T>> {}
 
-export type ProjectionFieldsInputType<T> = {[K in keyof T] : boolean | Partial<ProjectionFieldsInputType<T[K]>>}
+export type ProjectionFieldsInputType<T> = {
+  [K in keyof T]: boolean | Partial<ProjectionFieldsInputType<T[K]>>;
+};
 
 export interface ProjectionMethods<T, RES_TYPE> {
- /**
+  /**
    * Projection allows you to select the fields to be returned.
-   * 
+   *
    * @param projectionFields Fields you want to get as response.
-   * 
+   *
    * @returns Fields value for which you requested, If a requested field doesn’t exist in the projected object, the returned field value is set to null.
-   * 
+   *
    * @example
    * query.Address.all().project({ country: true }).exec();
    * OR
@@ -95,30 +157,34 @@ export interface ProjectionMethods<T, RES_TYPE> {
    * query.Address.byId('360427545614615075').delete().project({ country: true }).exec();
    * OR
    * query.Address.byId('360427545614615075').update({street: "Vijay Nagar Road"}).project({ country: true }).exec();
-   * 
+   *
    * @see {@link https://fqlx-beta--fauna-docs.netlify.app/fqlx/beta/reference/language/projection See more...}
    */
- project(projectionFields: Partial<ProjectionFieldsInputType<T>>): ExecMethods<RES_TYPE>;
+  project(
+    projectionFields: Partial<ProjectionFieldsInputType<T>>
+  ): ExecMethods<RES_TYPE>;
 }
 
-export interface FirstWhereMethods<T> extends ProjectionMethods<T, T>, ExecMethods<T> { }
+export interface DeleteMethods<T>
+  extends ProjectionMethods<T, T>,
+    PromisifyExecMethods<T> {}
 
-export interface FirstMethods<T> extends ProjectionMethods<T, T>, ExecMethods<T> { }
+export interface UpdateMethods<T>
+  extends ProjectionMethods<T, T>,
+    PromisifyExecMethods<T> {}
 
-export interface OrderMethods<T> extends  FqlxFirst<T>, ProjectionMethods<T, PaginateData<T>>, ExecMethods<PaginateData<T>> { }
-
-export interface DeleteMethods<T> extends ProjectionMethods<T, T>, PromisifyExecMethods<T> { }
-
-export interface UpdateMethods<T> extends ProjectionMethods<T, T>, PromisifyExecMethods<T> { }
-
-export interface AllMethods<T> extends FqlxOrder<T>, FqlxFirst<T>, ProjectionMethods<T, PaginateData<T>>, ExecMethods<PaginateData<T>> {
-   /**
+export interface AllMethods<T>
+  extends FqlxOrder<T>,
+    FqlxFirst<T>,
+    ProjectionMethods<T, PaginateData<T>>,
+    ExecMethods<PaginateData<T>> {
+  /**
    * first where method get the first matching value from the Set.
    *
    * @param {(inputCondition: ((data: T) => boolean) | string)} function takes in a document of type T and returns a boolean
    *
    * @returns {FirstWhereMethods<T>}  returns the first matching value in the Set, or null if the Set is empty or no values match.
-   * 
+   *
    * @example
    * query.Address.all().firstWhere((data) => data.country == 'uk').exec();
    * OR
@@ -126,7 +192,9 @@ export interface AllMethods<T> extends FqlxOrder<T>, FqlxFirst<T>, ProjectionMet
    *
    * @see {@link https://fqlx-beta--fauna-docs.netlify.app/fqlx/beta/reference/schema_entities/set/firstwhere#description See more...}
    */
-  firstWhere(inputCondition: ((data: T) => boolean) | string): FirstWhereMethods<T>;
+  firstWhere(
+    inputCondition: ((data: T) => boolean) | string
+  ): FirstWhereMethods<T>;
 
   /**
    * where method set a subset of matching Set values.
@@ -139,7 +207,7 @@ export interface AllMethods<T> extends FqlxOrder<T>, FqlxFirst<T>, ProjectionMet
    * query.Address.all().where((data) => data.country == 'uk').exec();
    * OR
    * query.Address.all().where(`(data) => data.${dynamicKey} == "${dynamicValueToCheck}"`).exec();
-   * 
+   *
    * @see {@link https://fqlx-beta--fauna-docs.netlify.app/fqlx/beta/reference/schema_entities/set/where#signature See more...}
    */
   where(inputCondition: ((data: T) => boolean) | string): WhereMethods<T>;
@@ -151,7 +219,9 @@ export interface PaginateData<T> {
   before?: string | null | undefined;
 }
 
-export interface ByIdMethods<T, U> extends ProjectionMethods<T, T>, PromisifyExecMethods<T> {
+export interface ByIdMethods<T, U>
+  extends ProjectionMethods<T, T>,
+    PromisifyExecMethods<T> {
   /**
    * The update() method updates the document with the object fields and returns the updated document.
    *
