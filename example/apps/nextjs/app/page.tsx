de80fb01e42aef12c7615321b02fd6d5eda3d91c
Madhav <madhav.kabra@dechea.com>
2023-06-28 05:27:15 +0000

    Update example application path

--- example/apps/nextjs/app/page.tsx
+++ example/apps/nextjs/app/page.tsx
@@ -6,9 +6,5 @@ import { useQuery } from 'fqlx-client';
 export default function Home() {
   const query = useQuery<Query>();
 
-  const implants = query.Product.all().where(p => p.implant != null).exec();
-
-  console.log({ implants });
-
   return <div>ksjhdkjsdh</div>;
 }
