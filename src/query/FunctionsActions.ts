f356b2e1703be283998c51e7f96d7e0b31b98dda
Aditya Vaishnav <firehudson@gmail.com>
2023-08-04 06:32:01 +0000

    Update function query params

--- src/query/FunctionsActions.ts
+++ src/query/FunctionsActions.ts
@@ -3,19 +3,24 @@
 import { promisedExecutor } from './executor';
 
 export class FunctionsActions<T> {
-  public call = (): { [key in keyof T]: '' } => {
+  public call = (): { [key in keyof T]: Function } => {
     return new Proxy(
       {},
       {
-        get(_target, functionName: string): Promise<any> {
-          // @ts-expect-error
-          return async (...args) => {
-            const query = `${functionName}(${args.join(' , ')})`;
+        get(_target, functionName: string) {
+          return async (...args: any): Promise<any> => {
+            const stringifiedArgs = JSON.stringify(args);
+            const params = stringifiedArgs.substring(
+              1,
+              stringifiedArgs.length - 1
+            );
+
+            const query = `${functionName}(${params})`;
 
             return await promisedExecutor(query);
           };
         },
       }
-    ) as { [key in keyof T]: '' };
+    ) as { [key in keyof T]: Function };
   };
 }
